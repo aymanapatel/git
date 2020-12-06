e1336c35cefab90e107b7fb44f75942812bdab3a
Ayman <ayman.patel97@gmail.com>
2020-12-06 20:15:39 +0530

    Python githook

--- install_hooks.bat
+++ install_hooks.bat
@@ -1,4 +1,3 @@
-  
 @echo off
 
 :: ====================================================================
@@ -12,4 +11,7 @@
 set LIB_DIR=submodules
 
 if exist .git\hooks\commit-msg del /F .git\hooks\commit-msg
-mklink .git\hooks\commit-msg ..\..\python\commit-msg.py
+mklink .git\hooks\commit-msg ..\..\src\commit-msg.py
+
+if exist .git\hooks\%LIB_DIR% rmdir /S /Q .git\hooks\%LIB_DIR%
+mklink /D .git\hooks\%LIB_DIR% ..\..\src\%LIB_DIR%
