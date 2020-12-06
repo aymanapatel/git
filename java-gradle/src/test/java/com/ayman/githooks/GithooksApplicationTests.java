eff579eb2381d293d03110193cae7517f1b0fd66
Ayman <ayman.patel97@gmail.com>
2020-12-06 11:01:25 +0530

    Move folder

---
+++ java-gradle/src/test/java/com/ayman/githooks/GithooksApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ayman.githooks;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//Assertions.fail("Fail test");
+	}
+
+}
