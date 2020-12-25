e290f5c50905e9d74925011c3dfff8f130b34997
Ayman Patel <ayman.patel97@gmail.com>
2020-12-25 09:41:07 +0530

    Add install_hooks.sh

--- githooks/python/install_hooks.bat
+++ githooks/python/install_hooks.bat
@@ -22,5 +22,5 @@ mklink ..\..\.git\hooks\commit-msg ..\..\githooks\python\commit-msg.py
 
 :: Curent directory: <root>\githooks\python
 :: Final directory:  <root>\.git\hooks
-if exist ..\.git\hooks\%LIB_DIR% rmdir /S /Q ..\..\.git\hooks\%LIB_DIR%
+if exist ..\..\.git\hooks\%LIB_DIR% rmdir /S /Q ..\..\.git\hooks\%LIB_DIR%
 mklink /D ..\..\.git\hooks\%LIB_DIR% ..\..\githooks\python\%LIB_DIR%
